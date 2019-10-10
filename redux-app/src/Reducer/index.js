ef99e43dbe55a9da24b2445dbe200844c67efd21
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-10 15:40:07 -0400

    Created store and reducer

---
+++ redux-app/src/Reducer/index.js
@@ -0,0 +1,15 @@
+
+const initialState ={
+    Quotes: [],
+};
+
+const reducer = (state = initialState, action) => {
+switch (action.type) {
+
+    default:
+        return state;
+}
+
+}
+
+export default reducer;
